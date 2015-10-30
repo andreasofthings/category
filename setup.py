f4bd96261dcac57ef56e69854433c0da8f7a5372
Andreas Neumeier <andreas@neumeier.org>
2015-10-30 09:32:21 +0100

    initial test.

---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup
+from category import __version__
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+setup(
+    name='category',
+    version=__version__,
+    packages=['category'],
+    include_package_data=True,
+    license='BSD License',    # example license
+    description='A category for a django model.',
+    long_description=README,
+    author='Andreas.Neumeier',
+    author_email='andreas@neumeier.org',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',    # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[
+        'django>=1.8.0',
+    ],
+    dependency_links=[
+    ]
+)
