d770b41874b822a11210fdca8a7430a14feb881b
Andreas Neumeier <andreas@neumeier.org>
2015-11-27 21:17:19 +0100

    changed the way to test. break it further.

--- tests/settings.py
+++ tests/settings.py
@@ -1,91 +1,15 @@
-#!/usr/bin/env
-# -*- encoding: utf-8
-# vim: ts=4 et sw=4 sts=4
+from defaults import *
 
-"""
-"""
-
-from django.conf.global_settings import *
-
-DEBUG = True
-TIME_ZONE = 'UTC'
-LANGUAGE_CODE = 'en-US'
-SITE_ID = 1
-USE_L10N = True
-USE_TZ = True
-
-SECRET_KEY = 'local'
+# Database for travis
+# https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myapp_test',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
-
-ROOT_URLCONF = 'tests.urls'
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
-
-MIDDLEWARE_CLASSES = [
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-TEMPLATE_CONTEXT_PROCESSORS = [
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.media',
-    'django.core.context_processors.static',
-    'django.core.context_processors.tz',
-    'django.core.context_processors.request',
-    'django.contrib.messages.context_processors.messages'
-]
-
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
-
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sites',
-
-    'category',
-    'tests',
-)
-
-PASSWORD_HASHERS = (
-    'django.contrib.auth.hashers.MD5PasswordHasher',
-)
-
-import django
-if django.VERSION < (1, 4):
-    TEMPLATE_CONTEXT_PROCESSORS.remove('django.core.context_processors.tz')
-    MIDDLEWARE_CLASSES.remove(
-        'django.middleware.clickjacking.XFrameOptionsMiddleware'
-    )
